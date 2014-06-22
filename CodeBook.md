90a28a9a83a94b688bcea0fbef9e21a778bd8927
okrahl <oliver.krahl@emailn.de>
2014-06-22 17:20:55 +0200

    Work complete. Ready for submission.

--- CodeBook.md
+++ CodeBook.md
@@ -1,9 +1,62 @@
-Download data manually from here:
+## Data to work on
 
-`https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip`
+The original data set is split into 28 files in 4 subdirectories. The file `README.txt` is self explanatory, the file `features_info.txt` explains the semantic of data fields.
 
-into project work directory:
+Skipping the `/Inertial Signals` subdirectories, the `dim()` Funktion gives the following results:
 
-`/path/to/coursera/getdata/project`
+    file                    rows columns #levels
+    activity_labels.txt        6       2      6
+    features.txt             561       2
+    subject_train.txt       7352       1     21
+    X_train.txt             7352     561
+    y_train.txt             7352       1      6
+    subject_test.txt        2947       1      9
+    X_test.txt              2947     561
+    y_test.txt              2947       1      6
 
-Change this to your local needs.
+    561(1), 477(2)
+The heading `#levels` indicates the number of levels (different values), found with the code snippet `length(levels(as.factor(subject_train[,1])))`. The 9 levels of `subject_test.txt` are `2, 4, 9, 10, 12, 13, 18, 20, 24`, together with the complementary 21 levels in`subject_train.txt` this results in a serie from 1 to 30. So these levels are the subject IDs.
+
+The 9 files in the subdirectory `/UCI HAR Dataset/train/Inertial Signals` all have 7352 rows by 128 columns, the 9 files in the subdirectory `UCI HAR Dataset/test/Inertial Signals` all have 2947 rows by 128 columns. The subdirectories `Inertial Signals` seem to hold raw data not matching the course projects requirements.
+
+Now it is obvious how these data fit together. The number of rows in `features.txt` matches the number of columns in `X_train.txt` and `X_test.txt`, so `features.txt` holds the column names. The number of levels in `y_train.txt` and `y_test.txt` matches the number of levels in `activity_labels.txt`, so the `y`-files hold the activity as integers, and `activity_labels.txt` maps these integers to descriptive labels. Combining these data into an intermediate table will result in a table with 10299 rows and 563 columns. 
+
+## Merging the training and the test sets to create one data set
+
+The 6 data files are merged in three steps:
+
+   1. `subject_train.txt`, `y_train.txt`, `X_train.txt` are combined to make a data.frame with 7352 rows and 563 columns for the training dataset using the `data.frame()`-Funktion.
+   2. `subject_test.txt`, `y_test.txt`, `X_test.txt` are combined to make a data.frame with 2947 rows and 563 columns for the test dataset using the `data.frame()`-Funktion.
+   3. the data.frames from step 1 and 2 are combined to make a data.frame with 10299 rows and 563 columns using the `rbind()`-Funktion
+
+## Extract only the measurements on the mean and standard deviation for each measurement
+
+Only the features with both mean and standard deviation are regarded, so features are extracted pairwise. Picking `tBodyAccJerk-std()-Y` for example results in complementary feature `tBodyAccJerk-mean()-Y` to extract, and vice versa. On the other hand `angle(Z,gravityMean)` is not extracted, because there is no feature `angle(Z,gravityStd)` or something alike.
+
+For filtering the features a roundtrip calculation takes place. Starting with the standard deviation all features will be regarded containing "std" in some way. From this a partner set of features is created by replacing the "std" substring with "mean". A lookup of this partner features in the original feature list will strip artificial "mean"-partners created by string replacement, but not included in the original measurements. The partner feature set after lookup has at most the same number of elements as the features of standard deviation we started with, maybe less. Now a backward substitution of "mean" back to "std" takes place to keep only those standard deviations, whose "mean" partner has passed the existence check.
+
+If the features contained two hypothetical values `angle-X-std()` and `angle-mean()-X`, but no `angle-X-mean()`, which would be the requested partner for `angle-X-std()`. Lookup of substitute will drop `angle-X-mean()` and the backward substitution will drop `angle-X-std()`. Maybe `angle-X-std()` and `angle-mean()-X` belong together, but we don't know, because they are violating a consistent naming scheme. Not being sure they appear pairwise, they are dropped both.
+
+## Descriptive activity names to name the activities in the data set
+
+This subtask is the most simple one. Build a data dictionary (hash table) from the content of file `activity_labels.txt`, so the descriptive activity name can be accessed by the integer activity as index. In this special case the integer activity in the file is identical to the row index, so the algorithm can be further simplified. Just access the data.frame from `activity_labels.txt` by the integer in the activity row of the extracted data. 
+
+## Appropriately label the data set with descriptive variable names
+
+In the file `features.txt` the variables contain special characters like `-()` etc. which have an own meaning in R, breaking standard functions on column names. The R function `make.names()` gives a clue about valid column names, but there are some quirks in the result. A sequence of special characters results in a sequence of dots in the variable name. These multiple dots elongate the name without much use.
+
+A decision has to be made about the flag indicating time or frequency domain of the measurement. As soon as velocity or acceleration are encountered, it is well known to encounter time as well, and frequency too as inverse of time. So the time and frequency flags are left as-is.
+
+As a personel bias I use lower case as often as possible, and I avoid camel case as often as possible. Encountering camel cases and translating to lower case will lose word boundaries and therefore readability. I decided to insert a single dot at word boundary and to translate to lower case. As a tradeoff the variable name is expanded.
+
+A full description of variable names is still given in the file `features_info.txt`
+
+## Average by subject and activity
+
+The intermediate result of the last step above is a data.frame with 10299 rows and 68 columns. Using `melt` and `cast` of package `reshape2` transforms this to a data.frame with 180 rows and 68 columns. The 180 rows are given by grouping the 30 subject identifiers (`id`) and the 6 activities. The certain number of rows with the same identifier and the same activity in the intermediate result will be aggregated into a single row in the final result. The aggregate function is the `mean()`-Funktion.
+
+## Result
+
+The `id` is the identifier of the subject who performed the activity, values are 1 to 30.
+The `activity` is self explanatory, values are WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING.
+The other 66 columns are the mean of measurements explained in `features_info.txt`, averaged by id and activity.
--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,186 @@
 # Getting and Cleaning Data Course Project
+library(reshape2)
 
 # change this to local work directory
 #projwd <- "/path/to/local/coursera/project/"
-projwd <- "/home/adam/Dokumente/proj/coursera/getting_and_cleaning_data"
-zipfile <- "getdata_projectfiles_UCI HAR Dataset.zip"
+#projwd <- "~/Dokumente/proj/coursera/getting_and_cleaning_data/courseproject"
+#setwd(projwd)
+
+# global constants
+k.zipfile <- "getdata_projectfiles_UCI HAR Dataset.zip"
+k.tidydata.file <- "tidydata.txt"
+k.dot <- "."
 
 # define functions
-merge.datasets <- function (datasets) {
-  
+read.data <- function (){
+  ## returns a list of data.frames each representing the contents of a file
+  file <- "UCI HAR Dataset/activity_labels.txt"
+  activity_labels <- read.table(file, header = FALSE, sep = "")
+  file <- "UCI HAR Dataset/features.txt"
+  features <- read.table(file, header = FALSE, sep = "")
+  # training data
+  file <- "UCI HAR Dataset/train/subject_train.txt"
+  subject_train <- read.table(file, header = FALSE, sep = "")
+#  colnames(subject_train) <- "id"
+  file <- "UCI HAR Dataset/train/X_train.txt"
+  x_train <- read.table(file, header = FALSE, sep = "")
+  file <- "UCI HAR Dataset/train/y_train.txt"
+  y_train <- read.table(file, header = FALSE, sep = "")
+#  colnames(y_train) <- "activity"  
+  # test data
+  file <- "UCI HAR Dataset/test/subject_test.txt"
+  subject_test <- read.table(file, header = FALSE, sep = "")
+#  colnames(subject_test) <- "id"
+  file <- "UCI HAR Dataset/test/X_test.txt"
+  x_test <- read.table(file, header = FALSE, sep = "")
+  file <- "UCI HAR Dataset/test/y_test.txt"
+  y_test <- read.table(file, header = FALSE, sep = "")
+#  colnames(y_test) <- "activity"  
+  ## read data for exploration
+  #file <- "UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt"
+  # body_acc_x_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt"
+  # body_acc_y_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt"
+  # body_acc_z_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt"
+  # body_gyro_x_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt"
+  # body_gyro_y_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt"
+  # body_gyro_z_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt"
+  # total_acc_x_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt"
+  # total_acc_y_train <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt"
+  # total_acc_z_train <- read.table(file, header = FALSE, sep = "")
+  # # test data
+  # file <- "UCI HAR Dataset/test/subject_test.txt"
+  # subject_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/X_test.txt"
+  # x_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/y_test.txt"
+  # y_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt"
+  # body_acc_x_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt"
+  # body_acc_y_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt"
+  # body_acc_z_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt"
+  # body_gyro_x_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt"
+  # body_gyro_y_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt"
+  # body_gyro_z_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt"
+  # total_acc_x_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt"
+  # total_acc_y_test <- read.table(file, header = FALSE, sep = "")
+  # file <- "UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt"
+  # total_acc_z_test <- read.table(file, header = FALSE, sep = "")  
+  return(list(activity_labels=activity_labels, features=features, 
+       subject_train=subject_train, x_train=x_train, y_train=y_train,
+       subject_test=subject_test, x_test=x_test, y_test=y_test))
 }
 
-convert.name <- function (name) {
+merge.datasets <- function (datasets) {
+  ## Build a single data.frame from 6 file contents
+  ## Input is a list of data.frames representing file contents
+  ## Return is a dataframe with 10299 rows by 563 columns, the two leading 
+  ## columns are subject id and activity followed by 561 feature columns
+  ## the first 7352 rows are from training data set, the following 2947 rows
+  ## are from test data set
+  train_col_complete <- data.frame(datasets$subject_train, 
+                                     datasets$y_train, datasets$x_train)
+  test_col_complete <- data.frame(datasets$subject_test, 
+                                    datasets$y_test, datasets$x_test)
+  x_all <- rbind(train_col_complete, test_col_complete)
+  return(x_all)
+}
+
+filter.features <- function(features) {
+  ## Input is a factor of length 561 with the measurement features as character
+  ## Return is a logical vector matching the pairwise features containing 
+  ## "std" and "mean"
+
+  # starting with features with standard deviation std
+  stdl <-  grepl("std", features)
+  stdfeat <- features[stdl]
+  std.to.mean <- gsub("std", "mean", stdfeat)
+  std.to.mean.logical <- features %in% std.to.mean
+  # roundtrip
+  #meanl <- grepl("mean", features)
+  #meanfeat <- features[meanl]
+  stdpartner <- features[std.to.mean.logical]
+  stdroundtrip <- gsub("mean", "std", stdpartner)
+  stdroundtripl <- features %in% stdroundtrip
+  return(stdroundtripl | std.to.mean.logical)
+}
+
+set.descriptive.activities <- function(df, filecontent){
+  ## replaces integer values in second column of input data
+  ## with descriptive names from file `activity_labels.txt
+  ## Input
+  ##   df: dataframe with integer values from 1 to 6 in second column
+  ##   filecontent: list of data.frames each element is content of a file
+  ## Returns a dataframe with descriptive activity names in column 2
+  act.labels <- filecontent$activity_labels[,2]
+  names(act.labels) <- filecontent$activity_labels[,1]
+  df[,2] <- act.labels[df[,2]]
+  return(df)
+}
+
+replace.camelcase <- function (camelcase.str, del = k.dot) {
+  # inserts the delimiter ahead of the uppercase letter in a camelcase string
+  #   http://stackoverflow.com/questions/8406974/splitting-camelcase-in-r
+  #
+  # Args:
+  #   camelcase.str: string with camelcase: CalculateAvgClicks, kConstantName
+  #   del: delimiter, to be inserted before uppercase letter
+  #
+  # Returns:
+  #   String with delimiter before each uppercase letter except first letter:
+  #     Calculate.Avg.Clicks, k.Constant.Name
+  tmp.str <- unlist(list(camelcase.str))
+  tmp.str <- gsub("^[^[:alnum:]]+|[^[:alnum:]]+$", "", tmp.str)
+  result <- gsub("(?!^)(?=[[:upper:]])", del, tmp.str, perl = TRUE)
+  return(result)
+}
+
+replace.repeating.delimiters <- function (rep.str, del = ".") {
+  # replaces a sequence of repeating delimiters in a string with a single 
+  # delimiter
+  #
+  # Args:
+  #   rep.str: string with sequence of repeating delimiters
+  #      abc...de.f, abc___de_f
+  #   del: delimiter
+  #
+  # Returns:
+  #   string with condensed sequence of delimiters
+  #     abc.de.f, abc_de_f
+  k.one.or.more <- "+"
+  esc.del <- c(k.dot)
+  if (del %in% esc.del) {
+    regex <- paste0("\\", del, k.one.or.more)
+  } else {
+    regex <- paste0(del, k.one.or.more)
+  }
+  result <- gsub(regex, del, rep.str)
+}
+
+convert.name <- function (name, del = ".") {
   # converts a name of a function or variable according to google styleguide
+  # except camelcase
   # https://google-styleguide.googlecode.com/svn/trunk/Rguide.xml
   #
   # Args:
   #   'name': character vector of length 1 representing the name of a
   #           variable in an unstardardized manner, 
   #           i.e. containing special characters
+  #   'del': delimiter depending on style guide
   #
   # Returns: 
   #   name according to google styleguide, that is: no special characters, 
@@ -27,86 +190,51 @@ convert.name <- function (name) {
   #   fBodyGyro-bandsEnergy()-49,64     f.body.gyro.bands.energy.49.64
   #   tBodyAcc-mean()-X                 t.body.acc.mean.x
   #   angle(tBodyGyroMean,gravityMean)  angle.t.body.gyro.mean.gravity.mean
-  # rules:
-  # strip all parenthesis pairs "()"
-  # replace all special characters with a dot: ()-,
-  # replace all upper case characters "U" with preceding dot: ".U"
+  
+  # replace all special characters with delimiter. Special characters: ()-,
+  #result <- gsub("[\\(\\),-]", del, name)
+  result <- make.names(name)
+  # replace camelcase with leading delimiter
+  #   http://stackoverflow.com/questions/8406974/splitting-camelcase-in-r
+  result <- replace.camelcase(result, del = k.dot)
+  # condense multiple delimiters to single delimiter
+  result <- replace.repeating.delimiters(result, del = k.dot)
   # translate to lower case
-  # replace all multiple dots with single dot
-  # strip trailing dot
-  result <- NULL
-  #result <- gsub()
+  result <- tolower(result)
   return(result)
 }
-source("convert_name.R")
-convert.name("fBodyGyro-bandsEnergy()-49,64..")
-convert.name("tBodyAcc-mean()-X")
-convert.name("angle(tBodyGyroMean,gravityMean)")
-convert.name("AenÖIHavet..")
-setwd(projwd)
-unzip(zipfile)
 
-# read data
-file <- "UCI HAR Dataset/activity_labels.txt"
-activity_labels <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/features.txt"
-features <- read.table(file, header = FALSE, sep = "")
-# training data
-file <- "UCI HAR Dataset/train/subject_train.txt"
-subject_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/X_train.txt"
-x_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/y_train.txt"
-y_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/body_acc_x_train.txt"
-body_acc_x_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/body_acc_y_train.txt"
-body_acc_y_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/body_acc_z_train.txt"
-body_acc_z_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/body_gyro_x_train.txt"
-body_gyro_x_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/body_gyro_y_train.txt"
-body_gyro_y_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/body_gyro_z_train.txt"
-body_gyro_z_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/total_acc_x_train.txt"
-total_acc_x_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/total_acc_y_train.txt"
-total_acc_y_train <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt"
-total_acc_z_train <- read.table(file, header = FALSE, sep = "")
-# test data
-file <- "UCI HAR Dataset/test/subject_test.txt"
-subject_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/X_test.txt"
-x_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/y_test.txt"
-y_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/body_acc_x_test.txt"
-body_acc_x_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/body_acc_y_test.txt"
-body_acc_y_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/body_acc_z_test.txt"
-body_acc_z_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/body_gyro_x_test.txt"
-body_gyro_x_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/body_gyro_y_test.txt"
-body_gyro_y_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/body_gyro_z_test.txt"
-body_gyro_z_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/total_acc_x_test.txt"
-total_acc_x_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/total_acc_y_test.txt"
-total_acc_y_test <- read.table(file, header = FALSE, sep = "")
-file <- "UCI HAR Dataset/test/Inertial Signals/total_acc_z_test.txt"
-total_acc_z_test <- read.table(file, header = FALSE, sep = "")
+# # test cases for function convert.name()
+# convert.name("fBodyGyro-bandsEnergy()-49,64..")
+# convert.name("tBodyAcc-mean()-X")
+# convert.name("angle(tBodyGyroMean,gravityMean)")
+# convert.name("AenÖIHavet..")
 
-# 
-x_col_complete_train <- data.frame(subject_train, y_train, x_train)
-x_col_complete_test <- data.frame(subject_test, y_test, x_test)
-x_all <- rbind(x_col_complete_train, x_col_complete_test)
-feature.names <- unlist(lapply(as.character(features[,2]), convert.name))
-names(x_all) <- c("subject", "activity", feature.names)
+## main program
+unzip(k.zipfile)
 
+filecontent <- read.data()
 
+# 1. Merge the training and the test sets to create one data set
+merged.data <- merge.datasets(filecontent)
+
+# 2. Extract only the measurements on the mean and standard deviation 
+#   for each measurement.
+features <- filecontent$features[,2]
+feat.filter <- filter.features(features)
+extracted.data <- merged.data[c(TRUE,TRUE, feat.filter)]
+
+# 3. Use descriptive activity names to name the activities in the data set
+prime.tidy.data <- set.descriptive.activities(extracted.data, filecontent)
+
+# 4. Appropriately label the data set with descriptive variable names
+feature.names <- unlist(lapply(as.character(features[feat.filter]), convert.name))
+names(prime.tidy.data) <- c("id", "activity", feature.names)
+
+# 5. Create a second, independent tidy data set with the average 
+#    of each variable for each activity and each subject.
+molten <- melt(prime.tidy.data, id.vars=c("id", "activity"))
+second.tidyset <- dcast(molten, id + activity ~ variable, mean)
+
+# write tidy data set to disc
+write.table(second.tidyset, k.tidydata.file, row.names=FALSE)
